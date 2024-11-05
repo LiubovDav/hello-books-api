7b12a3a9a98032df996d577d5007c250403efa5c
Liubov Dav <lubasha0512@gmail.com>
2024-11-04 16:58:20 -0800

    Added tests, changed book.py, conftest.py, test_book_routes.py

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -7,19 +7,18 @@ books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 @books_bp.post("")
 def create_book():
     request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
 
-    new_book = Book(title=title, description=description)
+    try:
+        new_book = Book.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
     db.session.add(new_book)
     db.session.commit()
 
-    response = {
-        "id": new_book.id,
-        "title": new_book.title,
-        "description": new_book.description,
-    }
-    return response, 201
+    return new_book.to_dict(), 201
 
 @books_bp.get("")
 def get_all_books():
@@ -33,31 +32,20 @@ def get_all_books():
     if description_param:
         query = query.where(Book.description.ilike(f"%{description_param}%"))
 
-    query = query.order_by(Book.id)
-    books = db.session.scalars(query)
+    books = db.session.scalars(query.order_by(Book.id))
     # We could also write the line above as:
     # books = db.session.execute(query).scalars()
 
     books_response = []
     for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
+        books_response.append(book.to_dict())
     return books_response
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
     book = validate_book(book_id)
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
-    }
+    return book.to_dict()
 
 def validate_book(book_id):
     try:
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app.db import db
 from flask.signals import request_finished
+from app.models.book import Book
 from dotenv import load_dotenv
 import os
 
@@ -30,3 +31,15 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(title="Ocean Book",
+                      description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                         description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    db.session.commit()
